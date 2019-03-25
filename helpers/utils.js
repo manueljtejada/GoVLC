f6180eea054762ec857e88211cb4367df2c9848c
Manuel Tejada <manute32@gmail.com>
2019-03-25 16:13:00 -0400

    Implement `titleCase` function

---
+++ helpers/utils.js
@@ -0,0 +1,9 @@
+export function titleCase(str) {
+  if (!str) return;
+  str = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(' ');
+}
