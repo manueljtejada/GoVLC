3ab4a596dbf4a65bfa2ee50a6b973f59e3093823
Manuel Tejada <manute32@gmail.com>
2019-04-04 21:51:02 +0200

    Show markers and user location on map view

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { MapView } from 'expo';
 import { connect } from 'react-redux';
+import { titleCase } from '../helpers/utils';
 
 class MapScreen extends Component {
   state = {
@@ -9,28 +11,46 @@ class MapScreen extends Component {
       longitude: -0.3823819,
       latitudeDelta: 0.0422,
       longitudeDelta: 0.0621,
-    }
-  }
+    },
+  };
 
-  onRegionChange = region => {
-    this.setState({ region });
-  }
+  goToPlaceDetail = place => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Place', {
+      place,
+    });
+  };
 
   render() {
+    const { places } = this.props;
+    const { region } = this.state;
+
+    if (!places) return;
+
     return (
       <MapView
         style={{ flex: 1 }}
-        region={this.state.region}
-        onRegionChange={this.onRegionChange}
-      />
+        provider="google"
+        initialRegion={region}
+        showsUserLocation
+        showsMyLocationButton
+      >
+        {places.map((marker, index) => (
+          <MapView.Marker key={index} coordinate={marker.coordinates}>
+            <MapView.Callout onPress={() => this.goToPlaceDetail(marker)}>
+              <Text>{titleCase(marker.properties.nombre)}</Text>
+              <Text>{titleCase(marker.address)}</Text>
+            </MapView.Callout>
+          </MapView.Marker>
+        ))}
+      </MapView>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    places: state.places
-  }
-}
+const mapStateToProps = state => ({
+  places: state.places,
+});
 
 export default connect(mapStateToProps)(MapScreen);
