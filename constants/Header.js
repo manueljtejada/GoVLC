4d3192570a5daa1c22c0c614f485b4e5bb852faf
Manuel Tejada <manute32@gmail.com>
2019-04-05 22:16:29 +0200

    Add base header styles

---
+++ constants/Header.js
@@ -0,0 +1,14 @@
+import Colors from './Colors';
+
+export default {
+  headerStyle: {
+    backgroundColor: Colors.tintColor,
+  },
+  headerTitleStyle: {
+    color: '#fff',
+  },
+  headerBackTitleStyle: {
+    color: '#fff',
+  },
+  headerTintColor: '#fff',
+};
