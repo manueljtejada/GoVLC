6c7696c0e5f17bbeec5ef63c1af2dd679b0ca37b
Manuel Tejada <manute32@gmail.com>
2019-04-06 17:13:50 +0200

    Add filter modal button to list screen

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, FlatList, TouchableWithoutFeedback } from 'react-native';
+import { ListItem, SearchBar, Icon } from 'react-native-elements';
+import { Permissions, Location } from 'expo';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../redux/actions/index';
 
-import { titleCase } from '../helpers/utils';
+import Filters from '../components/Filters';
 
-import Header from '../components/Header';
+import { titleCase } from '../helpers/utils';
+import Header from '../constants/Header';
 
 class ListScreen extends Component {
-  static navigationOptions = {
-    header: props => <Header {...props} />,
+  static navigationOptions = ({ navigation }) => {
+    const { params = {} } = navigation.state;
+    return {
+      title: 'GoVLC',
+      headerRight: (
+        <TouchableWithoutFeedback onPress={() => params.openFilters(true)}>
+          <Icon name="filter-list" color="#fff" />
+        </TouchableWithoutFeedback>
+      ),
+      ...Header,
+    };
   };
+
+  state = { search: '', modalVisible: false };
+
+  componentDidMount() {
+    this.props.navigation.setParams({
+      openFilters: this.setModalVisible,
+    });
+
+    // Get user location
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    const { setUserLocation } = this.props;
+
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    setUserLocation(location);
+  };
+
+  setModalVisible = visible => {
+    this.setState({ modalVisible: visible });
+  };
+
+  getItemKey = (item, index) => index.toString();
 
   goToPlaceDetail = place => {
     const { navigation } = this.props;
@@ -22,7 +64,13 @@ class ListScreen extends Component {
     });
   };
 
-  getItemKey = (item, index) => index.toString();
+  updateSearch = search => {
+    const { searchPlaces } = this.props;
+
+    this.setState({ search });
+
+    searchPlaces(search);
+  };
 
   renderItem = ({ item }) => (
     <ListItem
@@ -36,11 +84,22 @@ class ListScreen extends Component {
 
   render() {
     const { places } = this.props;
+    const { search, modalVisible } = this.state;
 
     if (!places) return;
 
     return (
       <View>
+        <Filters
+          setModalVisible={this.setModalVisible}
+          modalVisible={modalVisible}
+        />
+        <SearchBar
+          placeholder="Search for monuments..."
+          onChangeText={this.updateSearch}
+          value={search}
+          platform="ios"
+        />
         <FlatList
           data={places}
           keyExtractor={this.getItemKey}
