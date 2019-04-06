0994b42dfe444cb24f36527a20cecb9d4a0e5a11
Manuel Tejada <manute32@gmail.com>
2019-04-06 17:16:56 +0200

    Add styles constants

---
+++ constants/Styles.js
@@ -0,0 +1,18 @@
+export default {
+  fill: {
+    flex: 1,
+  },
+  container: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+};
