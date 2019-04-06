1673291b3f45ca7810727dc6502e85a32892a0ae
Manuel Tejada <manute32@gmail.com>
2019-04-06 17:12:12 +0200

    Add helper function to calculate distance between 2 coordinates

--- helpers/utils.js
+++ helpers/utils.js
@@ -7,3 +7,21 @@ export function titleCase(str) {
   }
   return str.join(' ');
 }
+
+export function distance(lat1, lon1, lat2, lon2) {
+  const R = 6371;
+
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.asin(Math.sqrt(a));
+
+  const d = R * c;
+  return d;
+}
