33708a8db52755a3f00c52d71f3f25274ab0687f
Manuel Tejada <manute32@gmail.com>
2019-04-11 18:58:38 +0200

    Fix colors import

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -12,7 +12,6 @@ import Filters from '../components/Filters';
 import { titleCase, distance } from '../helpers/utils';
 import Header from '../constants/Header';
 import Colors from '../constants/Colors';
-import Styles from '../constants/Styles';
 
 class ListScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -133,7 +132,7 @@ class ListScreen extends Component {
     <ListItem
       title={titleCase(item.properties.nombre)}
       subtitle={titleCase(item.address)}
-      subtitleStyle={{ color: Styles.grayLight }}
+      subtitleStyle={{ color: Colors.grayLight }}
       onPress={() => this.goToPlaceDetail(item)}
       bottomDivider
       chevron
