2403fcf1fd099b2044980143bc306eb562817154
Manuel Tejada <manute32@gmail.com>
2019-04-12 20:21:10 +0200

    Add lightbox to imagelist

--- components/ImageList.js
+++ components/ImageList.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Image } from 'react-native-elements';
 import PropTypes from 'prop-types';
+import Lightbox from 'react-native-lightbox';
 
 import Styles from '../constants/Styles';
 
-const ImageList = ({ images }) => (
-  <View style={Styles.row}>
-    {images.map(img => (
-      <Image
-        key={img.id}
-        source={{ uri: img.uri }}
-        style={{ width: 75, height: 75 }}
-      />
-    ))}
-  </View>
-);
+class ImageList extends Component {
+  render() {
+    const { images } = this.props;
+    return (
+      <View style={Styles.row}>
+        {images.map(img => (
+          <Lightbox key={img.id}>
+            <Image
+              source={{ uri: img.uri }}
+              style={{ width: 75, height: 75 }}
+            />
+          </Lightbox>
+        ))}
+      </View>
+    );
+  }
+}
 
 ImageList.propTypes = {
   images: PropTypes.arrayOf(PropTypes.object),
